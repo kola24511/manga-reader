a68fb6f3fca3624a87dd17ba7f00ff5e7a795498
kola24511 <kolyanemovdev@gmail.com>
2025-01-29 02:00:14 +0300

    Добавил сидер и фабрику автора

---
+++ database/factories/AuthorFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Database\Factories\Administration\FlightFactory;
+use App\Models\Entity\Author;
+
+class AuthorFactory extends Factory
+{
+    protected $model = Author::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name()
+        ];
+    }
+}
