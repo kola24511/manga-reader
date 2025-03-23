e2839dc5f21f116db7910c51508498f4e4ef57b0
kola24511 <kolyanemovdev@gmail.com>
2025-03-23 13:48:41 +0300

    Ресурсы для админки

---
+++ app/Filament/Resources/BookmarkStatusResource/Pages/CreateBookmarkStatus.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\BookmarkStatusResource\Pages;
+
+use App\Filament\Resources\BookmarkStatusResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateBookmarkStatus extends CreateRecord
+{
+    protected static string $resource = BookmarkStatusResource::class;
+}
